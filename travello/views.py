c320c4f35d42dc9a1f8f8b8d8ed6043b3bc7038c
alyohinadhikari@gmail.com <alyohinadhikari@gmail.com>
2020-02-05 17:15:49 +0545

    django sample

---
+++ travello/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import destinationdemo
+from django.core.files.storage import FileSystemStorage
+
+
+# Create your views here.
+def index(request):
+    dests = destinationdemo.objects.all()
+    return render(request,'index.html',{'dests':dests})
